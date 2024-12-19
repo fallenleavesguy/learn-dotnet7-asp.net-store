0ffe150cfac95b21469c73d710552097aa2c9085
donghs <no@email.com>
2024-12-19 19:52:39 +0800

    feat: add initial data

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -8,11 +8,15 @@ builder.Services.AddDbContext<StoreDbContext>(opts =>
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
 
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
 app.MapDefaultControllerRoute();
+
+SeedData.EnsurePopulated(app);
 
 app.Run();
