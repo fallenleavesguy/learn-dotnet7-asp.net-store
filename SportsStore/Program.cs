d2cdc4487dbe754ae3fd9808e81f7b27f2132db7
fallenleaves256 <9261562+fallenleavesguy@users.noreply.github.com>
2025-03-15 13:56:52 +0800

    feat: implement Session Cart

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 builder.Services.AddRazorPages();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
+builder.Services.AddScoped(SessionCart.GetCart);
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 var app = builder.Build();
 
