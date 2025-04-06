37ea31e5e04c54a7a90012f4a6172df54f2e1de5
fallenleaves256 <9261562+fallenleavesguy@users.noreply.github.com>
2025-04-06 19:53:48 +0800

    9.3.5 Completing the order controller

---
+++ SportsStore/Models/IOrderRepository.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SportsStore.Models;
+
+public interface IOrderRepository
+{
+  IQueryable<Order> Orders { get; }
+  void SaveOrder(Order order);
+}
---
+++ SportsStore/reset-db.cmd
@@ -0,0 +1,2 @@
+dotnet ef database drop --force --context StoreDbContext
+dotnet ef database update --context StoreDbContext
